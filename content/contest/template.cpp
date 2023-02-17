32d164d744ac7fd6061a3c487a7afa46950992f0
Minh Le <qminh279@gmail.com>
2023-02-16 21:41:21 -0500

    update new

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -5,13 +5,16 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int,int> pi;
 typedef tuple<int, int, int> iii;
+#define REP(i, a, b) for (int i = a; i < b; i++)
 #define MOD 1000007
-#define F first
-#define S second
+#define f first
+#define s second
 #define PB push_back
 #define MP make_pair
 #define MAX 100
 #define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
---
+++ content/minhle/RMQ-ST.h
@@ -0,0 +1,24 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2018-07-15
+ * License: CC0
+ * Source: Wikipedia
+ * Description: RMQ problem using Sparse Table, DP
+ * Time: Preprocess: O(N\log N), Querry: O(1)
+ * Status: 
+ */
+
+void process2(int M[MAXN][LOGMAXN], int A[MAXN], int N)
+{
+  int i, j;
+
+  for (i = 0; i < N; i++)
+    M[i][0] = i;
+
+  for (j = 1; 1 << j <= N; j++)
+    for (i = 0; i + (1 << j) - 1 < N; i++)
+      if (A[M[i][j - 1]] < A[M[i + (1 << (j - 1))][j - 1]])
+        M[i][j] = M[i][j - 1];
+      else
+        M[i][j] = M[i + (1 << (j - 1))][j - 1];
+}
