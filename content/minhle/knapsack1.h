334d74b5bf1743a6d6016e30af4d6439301af6d7
Minh Le <qminh279@gmail.com>
2023-02-25 12:06:01 -0500

    FINAL FINAL

---
+++ content/minhle/knapsack1.h
@@ -0,0 +1,32 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2018-07-15
+ * License: CC0
+ * Source: Wikipedia
+ * Description: value range is large and weight range is small
+ * Time: O(\sqrt(N)) 
+ * Status: 
+ */
+
+const int mx = 1e5+1;
+
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0;
+}
+
+long long dp[101][mx];
+int w[101], v[101];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N, W; cin >> N >> W;
+    for(int i = 0; i < N; ++i) cin >> w[i] >> v[i];
+
+    for(int i = 0; i < N; ++i) for(int j = 0; j <= W; ++j) {
+        if(j + w[i] <= W) ckmax(dp[i + 1][j + w[i]], dp[i][j] + v[i]);
+        ckmax(dp[i + 1][j], dp[i][j]);
+    }
+
+    cout << dp[N][W] << endl;
+}
